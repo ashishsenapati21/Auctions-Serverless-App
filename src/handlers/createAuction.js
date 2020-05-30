e10112742ef3ace5a68eaee0700a5cba03b79d80
Ashish Kumar Senapati <ashishsenapati21@gmail.com>
2020-05-30 11:50:38 +0530

    initial commit

---
+++ src/handlers/createAuction.js
@@ -0,0 +1,42 @@
+import {v4 as uuid} from 'uuid';
+import AWS from 'aws-sdk';
+import commonMidware from '../lib/midware'
+import createError from 'http-errors';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function createAuction(event, context) {
+  const { title } = event.body;
+  const now = new Date();
+  const end = new Date();
+  end.setHours(now.getHours() + 1);
+
+  const auction = {
+    id: uuid(),
+    title,
+    highestBid: {
+      amount: 0
+    },
+    status: 'OPEN',
+    createdAt: now.toISOString(),
+    endingAt: end.toISOString(),
+  };
+
+  try {
+    await dynamodb.put({
+      TableName: process.env.AUCTIONS_TABLE_NAME,
+      Item: auction,
+    }).promise();
+  } catch(error) {
+    throw new createError.InternalServerError(error);
+  }
+  
+  return {
+    statusCode: 200,
+    body: JSON.stringify(auction),
+  };
+}
+
+export const handler = commonMidware(createAuction)
+
+
---
+++ src/handlers/getAuction.js
@@ -0,0 +1,40 @@
+import AWS from 'aws-sdk';
+import commonMidware from '../lib/midware'
+import createError from 'http-errors';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function getAuction(event, context) {
+  const { status } = event.queryStringParameters;
+  let auction;
+
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    IndexName: 'statusAndEndDate',
+    KeyConditionExpression: '#status = :status',
+    ExpressionAttributeValues: {
+      ':status': status
+    },
+    ExpressionAttributeNames: {
+      '#status':'status'
+    }
+  };
+
+  try {
+      const result = await dynamodb.query(params).promise();
+
+      auction = result.Items; 
+
+  } catch (error) {
+    throw new createError.InternalServerError(error);
+  }
+  
+  return {
+    statusCode: 200,
+    body: JSON.stringify(auction),
+  };
+}
+
+export const handler = commonMidware(getAuction)
+
+
